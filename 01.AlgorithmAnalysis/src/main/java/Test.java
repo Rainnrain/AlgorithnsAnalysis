0210bd524053cfa12b5208fba7d4d2f516d4b81b
Lorraine <lorrainecannom@gmail.com>
2022-08-29 11:34:18 -0400

    Two sum solution

--- 01.AlgorithmAnalysis/src/main/java/Test.java
+++ 01.AlgorithmAnalysis/src/main/java/Test.java
@@ -1,29 +1,36 @@
+import java.util.Arrays;
+
 public class Test {
     public static void main(String[] args) {
         long startTime;
         long endTime;
 
         startTime = System.currentTimeMillis();
-        System.out.println("Time before:"+ startTime);
+        System.out.println("Time before:" + startTime);
 
         endTime = System.currentTimeMillis();
-        System.out.println("End time: "+endTime);
-        System.out.println("Total time: "+ (endTime-startTime) +" miliseconds");
+        System.out.println("End time: " + endTime);
+        System.out.println("Total time: " + (endTime - startTime) + " miliseconds");
 
-        int [] array=new int[] {2,7,9,11};
+        int[] array = new int[]{4, 7, 2, 11};
+
+        System.out.println(Arrays.toString(twoSumSolution1(array, 9)));
     }
 
-    public static int[] twoSumSolution1(int [] array, int targetValue) {
+    public static int[] twoSumSolution1(int[] array, int targetValue) {
 
 
         for (int i = 0; i < array.length; i++) {
 
-            for (int j = 0; j < array.length; j++) {
-                if (array[i] + array[j] == targetValue) {
-                   return  new int []{i ,j} ;
+            for (int j = 1; j < array.length; j++) {
+                if (i != j) {
+                    if (array[i] + array[j] == targetValue) {
+                        return new int[]{i, j};
+                    }
                 }
             }
         }
-        return new int[]{};
-    }
+            return new int[]{};
+        }
+
 }
