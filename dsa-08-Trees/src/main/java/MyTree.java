10b1f0271c178443dd515a0603e0924e3fdaf521
Lorraine <lorrainecannom@gmail.com>
2022-10-30 16:05:29 -0400

    Tree methods

--- dsa-08-Trees/src/main/java/MyTree.java
+++ dsa-08-Trees/src/main/java/MyTree.java
@@ -1,3 +1,6 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class MyTree {
     TNode root;
 
@@ -27,6 +30,50 @@ public class MyTree {
                 }
                 current=current.rightChild;
             }
+        }
+    }
+
+    // preOrdered Transversal of the tree
+    //ROOT-LEFT-RIGHT
+    void preOrderTraversal(TNode root){
+        if(root==null) return;
+        System.out.print(root.value+",");
+        preOrderTraversal(root.leftChild);
+        preOrderTraversal(root.rightChild);
+
+    }
+
+    void inOrderTraversal(TNode root){
+        if (root==null) return;
+        inOrderTraversal(root.leftChild);
+        System.out.print(root.value+",");
+        inOrderTraversal(root.rightChild);
+    }
+
+    void postOrderTraversal(TNode root){
+        if(root==null) return;
+        inOrderTraversal(root.leftChild);
+        inOrderTraversal(root.rightChild);
+        System.out.print(root.value+",");
+    }
+
+    void levelOrderTranversal(){
+        if(root==null) return;
+        Queue<TNode> queue = new LinkedList<>();
+
+        queue.add(root);
+
+        while(!queue.isEmpty()){
+
+            TNode toVisit=queue.poll(); // gets front element of the queue
+            System.out.println(toVisit.value+" , ");
+
+            if(toVisit.leftChild!=null){
+                queue.add(toVisit.leftChild);
+            }if(toVisit.rightChild!=null){
+                queue.add(toVisit.rightChild);
+            }
+
         }
     }
 }
